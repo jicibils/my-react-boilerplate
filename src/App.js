c49c6bee586aeca2e864b4e3b71941e6ba77352e
jicibils <jicibils@hotmail.com>
2019-06-08 19:45:00 -0300

    test commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacts
+          Learn React
         </a>
       </header>
     </div>
